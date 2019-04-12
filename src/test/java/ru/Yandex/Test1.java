df2bb81589c639cccc4a2c227bba94c345286a71
Konst <k.pro1966@mail.ru>
2019-04-13 00:39:13 +0400

    Init 3

--- src/test/java/ru/Yandex/Test1.java
+++ src/test/java/ru/Yandex/Test1.java
@@ -16,12 +16,33 @@ public class Test1 extends WebDriverSetup{
         driver.get("https://yandex.ru");
         WebDriverWait wait = new WebDriverWait(driver,10);
         wait.until(ExpectedConditions.visibilityOf(driver.findElementByLinkText("Маркет")));
-        driver.findElementByLinkText("Маркет").sendKeys(Keys.ENTER);
+        //driver.findElementByLinkText("Маркет").sendKeys(Keys.ENTER);
         driver.findElementByLinkText("Компьютерная техника").sendKeys(Keys.ENTER);
         driver.findElementByLinkText("Ноутбуки").sendKeys(Keys.ENTER);
         //driver.findElementById("glpriceto").sendKeys("30000");
         driver.findElementByLinkText("HP").click();
         driver.findElementByLinkText("Lenovo").click();
+
+        int count = driver.findElementsByClassName("link n-link_theme_blue i-bem link_js_inited").size();
+        System.out.print("Элементов");
+        System.out.println(count);
+        Assert.assertEquals(12,count);
+
+    }
+
+    @Test
+    public void SecondTest() {
+
+        driver.get("https://yandex.ru");
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElementByLinkText("Маркет")));
+        //driver.findElementByLinkText("Маркет").sendKeys(Keys.ENTER);
+        driver.findElementByLinkText("Компьютерная техника").sendKeys(Keys.ENTER);
+        driver.findElementByLinkText("Планшеты").sendKeys(Keys.ENTER);
+        //driver.findElementById("glpriceto").sendKeys("30000");
+        driver.findElementByLinkText("Acer").click();
+        driver.findElementByLinkText("DELL").click();
+
         int count = driver.findElementsByClassName("link n-link_theme_blue i-bem link_js_inited").size();
         System.out.print("Элементов");
         System.out.println(count);
