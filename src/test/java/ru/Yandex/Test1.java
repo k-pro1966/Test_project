f0234d5c2a526b8258b4eafd41666980ea12f11c
Konst <k.pro1966@mail.ru>
2019-04-12 23:42:43 +0400

    Init 3

--- src/test/java/ru/Yandex/Test1.java
+++ src/test/java/ru/Yandex/Test1.java
@@ -1,5 +1,8 @@
 package ru.Yandex;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.By.ById;
@@ -13,24 +16,35 @@ import java.util.concurrent.TimeUnit;
 
 public class Test1 {
     private static WebDriverWait wait;
-    @Test
-    public void FirstTest() {
-        
-        System.setProperty("webdriver.ie.driver","C:/Users/Konst/IdeaProjects/IEDriverServer.exe");
-        InternetExplorerDriver driver = new InternetExplorerDriver();
+    public InternetExplorerDriver driver;
+
+    @Before
+    public void Setup() {
+        System.setProperty("webdriver.ie.driver","C:/Users/Konst/IdeaProjects/Test5Project/IEDriverServer.exe");
+        driver = new InternetExplorerDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void FirstTest() {
         driver.get("https://yandex.ru");
-        //driver.findElement(new ByIdOrName("market")).sendKeys(Keys.ENTER);
         driver.findElementByLinkText("Маркет").sendKeys(Keys.ENTER);
         driver.findElementByLinkText("Компьютерная техника").sendKeys(Keys.ENTER);
         driver.findElementByLinkText("Ноутбуки").sendKeys(Keys.ENTER);
         driver.findElementById("glpriceto").sendKeys("30000");
         driver.findElementByLinkText("HP").click();
         driver.findElementByLinkText("Lenovo").click();
-        driver.findElementByClassName("link n-link_theme_blue i-bem link_js_inited").
-        //https://market.yandex.ru/?clid=505&utm_source=face_abovesearch&utm_campaign=face_abovesearch
+        int count = driver.findElementsByClassName("link n-link_theme_blue i-bem link_js_inited").size();
+        System.out.print("Элементов");
+        System.out.println(count);
+        Assert.assertEquals(12,count);
+
+    }
+
+    @After
+    public void Close() {
         driver.quit();
     }
 }
