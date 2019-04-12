2dd3bf847efdb8bbe90c322e2c2dbe947170a51a
Konst <k.pro1966@mail.ru>
2019-04-13 00:30:30 +0400

    Init 3

--- src/test/java/ru/Yandex/Test1.java
+++ src/test/java/ru/Yandex/Test1.java
@@ -1,35 +1,21 @@
 package ru.Yandex;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.By.ById;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ByIdOrName;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
+public class Test1 extends WebDriverSetup{
 
-public class Test1 {
-    //private static WebDriverWait wait;
-    public InternetExplorerDriver driver;
-
-    @Before
-    public void Setup() {
-        System.setProperty("webdriver.ie.driver","C:/Users/Konst/IdeaProjects/Test_Project/IEDriverServer.exe");
-        driver = new InternetExplorerDriver();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        //wait = new WebDriverWait(driver, 10);
-    }
 
     @Test
     public void FirstTest() {
+
         driver.get("https://yandex.ru");
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElementByLinkText("Маркет")));
         driver.findElementByLinkText("Маркет").sendKeys(Keys.ENTER);
         driver.findElementByLinkText("Компьютерная техника").sendKeys(Keys.ENTER);
         driver.findElementByLinkText("Ноутбуки").sendKeys(Keys.ENTER);
@@ -43,8 +29,4 @@ public class Test1 {
 
     }
 
-    @After
-    public void Close() {
-        driver.quit();
-    }
 }
---
+++ src/test/java/ru/Yandex/WebDriverSetup.java
@@ -0,0 +1,25 @@
+package ru.Yandex;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class WebDriverSetup {
+
+    public InternetExplorerDriver driver;
+
+    @Before
+    public void Setup() {
+        System.setProperty("webdriver.ie.driver","C:/Users/Konst/IdeaProjects/Test_Project/IEDriverServer.exe");
+        driver = new InternetExplorerDriver();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        //wait = new WebDriverWait(driver, 10);
+    }
+
+    @After
+    public void Close() {
+        driver.quit();
+    }
+
+}
