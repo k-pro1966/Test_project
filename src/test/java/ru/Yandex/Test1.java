9e736de8a18a5b7c83be4fa3b7ecde41bae6c6b8
Konst <k.pro1966@mail.ru>
2019-04-12 23:47:59 +0400

    Init 3

--- src/test/java/ru/Yandex/Test1.java
+++ src/test/java/ru/Yandex/Test1.java
@@ -15,16 +15,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
 public class Test1 {
-    private static WebDriverWait wait;
+    //private static WebDriverWait wait;
     public InternetExplorerDriver driver;
 
     @Before
     public void Setup() {
-        System.setProperty("webdriver.ie.driver","C:/Users/Konst/IdeaProjects/Test5Project/IEDriverServer.exe");
+        System.setProperty("webdriver.ie.driver","C:/Users/Konst/IdeaProjects/Test_Project/IEDriverServer.exe");
         driver = new InternetExplorerDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, 10);
+        //wait = new WebDriverWait(driver, 10);
     }
 
     @Test
@@ -33,7 +33,7 @@ public class Test1 {
         driver.findElementByLinkText("Маркет").sendKeys(Keys.ENTER);
         driver.findElementByLinkText("Компьютерная техника").sendKeys(Keys.ENTER);
         driver.findElementByLinkText("Ноутбуки").sendKeys(Keys.ENTER);
-        driver.findElementById("glpriceto").sendKeys("30000");
+        //driver.findElementById("glpriceto").sendKeys("30000");
         driver.findElementByLinkText("HP").click();
         driver.findElementByLinkText("Lenovo").click();
         int count = driver.findElementsByClassName("link n-link_theme_blue i-bem link_js_inited").size();
